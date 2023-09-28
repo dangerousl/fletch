13696c0275a2364ad321255f7ed57979c2175b44
Lydell <llnewby3@gmail.com>
2023-09-27 23:18:54 -0500

    added a handler service, more legit config options, testing service

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,17 +1,39 @@
 use serde::Deserialize;
+use std::collections::HashMap;
 
 pub mod parser;
 
 // ProxyConfig is the top-level configuration struct for the proxy
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ProxyConfig {
+    pub proxy_port: u16,
+    pub routes: HashMap<String, RouteConfig>,
+}
+
+// RouteConfig is the configuration struct for a given route, defined as a String key in the routes HashMap
+#[derive(Debug, Deserialize, Clone)]
+pub struct RouteConfig {
+    pub host: String,
     pub port: u16,
     pub filters: Vec<FilterConfig>,
 }
 
 // FilterConfig is the configuration struct for a given filter... maybe
-#[derive(Debug, Deserialize)]
-pub struct FilterConfig {
-    pub path: String,
-    pub action: String,
+#[derive(Debug, Deserialize, Clone)]
+#[serde(tag = "type")]
+pub enum FilterConfig {
+    HeaderAdder(HeaderAdderConfig),
+    // Add more Soon™
+}
+
+// HeaderAdderConfig is the configuration struct for the HeaderAdder filter
+// Example usage:
+// filters:
+//   - type: "HeaderAdder"
+//     header_key: "X-My-Header"
+//     header_value: "my_value"
+#[derive(Debug, Deserialize, Clone)]
+pub struct HeaderAdderConfig {
+    pub header_key: String,
+    pub header_value: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,57 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
+use std::sync::Arc;
+use std::sync::Mutex;
 
+use config::ProxyConfig;
 use http_body_util::Full;
 use hyper::body::Bytes;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
+use lazy_static::lazy_static;
 use tokio::net::TcpListener;
 
 mod config;
 use config::parser::load_config;
 
+// global config variable, essentially
+lazy_static! {
+    static ref CONFIG: Mutex<Option<Arc<ProxyConfig>>> = Mutex::new(None);
+}
+
 // derived heavily from the Hyper example
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
-    // Load the config, just print it. that's enough for tonight
     // because of the "await," this function will wait for the config to load before continuing to bind the listener
-    let config = load_config("src/test_config.yaml").await?;
+    // tldr: sets the global CONFIG variable to the loaded ProxyConfig object, 
+    // making it accessible to the rest of the application in a thread-safe manner.
+    let config = Arc::new(load_config("src/test_config.yaml").await?);
+    {
+        let mut config_ref = CONFIG.lock().unwrap();
+        *config_ref = Some(config.clone());
+    }
+
+    // gotta print the config at start up, right?
     println!("Config: {:?}", config);
 
     // create a TcpListener and bind it to addr
-    let addr = SocketAddr::from(([127, 0, 0, 1], config.port));
+    let addr = SocketAddr::from(([127, 0, 0, 1], config.proxy_port));
     let listener = TcpListener::bind(addr).await?;
 
     // loop continuously accept incoming connections
     loop {
         let (stream, _) = listener.accept().await?;
-
-        // TODO: I am not sure what this means yet.
-        // Use an adapter to access something implementing `tokio::io` traits as if they implement
-        // `hyper::rt` IO traits.
         let io = TokioIo::new(stream);
-
+    
+        let config_clone = config.clone(); // clone the Arc to move into the tokio task
         // Spawn a tokio task to serve multiple connections concurrently
         tokio::task::spawn(async move {
-            // bind the incoming connection to our `hello` service, this is just to get us started
+            let hs = HandlerService { config: config_clone }; // Create a new HandlerService here
             if let Err(err) = http1::Builder::new()
-                .serve_connection(io, service_fn(hello))
+                .serve_connection(io, service_fn(|req| hs.call(req)))
                 .await
             {
                 println!("Error serving connection: {:?}", err);
@@ -47,6 +60,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     }
 }
 
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Fletch, or Fern?!"))))
+#[derive(Clone)]
+struct HandlerService {
+    config: Arc<ProxyConfig>,  // Use Arc to make proxyConfig shareable among tasks
+}
+
+impl HandlerService {
+    async fn call(&self, req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+        let uri = req.uri().path();
+
+        if let Some(route) = self.config.routes.get(uri) {
+            // a simluation of a proxy request
+            let resp_body = format!("Routing to {}:{}", route.host, route.port);
+            Ok(Response::new(Full::new(Bytes::from(resp_body))))
+        } else {
+            Ok(Response::new(Full::new(Bytes::from("Route not found"))))
+        }
+    }
 }
