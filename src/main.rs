0178a5d7417b196c90f58b0eb7cad4a63bf36656
Lydell <llnewby3@gmail.com>
2023-09-28 23:35:14 -0500

    added a test header adder

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
+use std::str::FromStr;
 use std::sync::Arc;
 use std::sync::Mutex;
 
+use config::FilterConfig;
+use config::HeaderAdderConfig;
 use config::ProxyConfig;
+use http::HeaderName;
 use http_body_util::Full;
+use http::header::HeaderValue;
 use hyper::body::Bytes;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
@@ -45,14 +50,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     loop {
         let (stream, _) = listener.accept().await?;
         let io = TokioIo::new(stream);
-
-        // clone the Arc to move into the tokio task
-        let config_clone = config.clone();
-        
+    
+        let config_clone = config.clone(); // clone the Arc to move into the tokio task
         // Spawn a tokio task to serve multiple connections concurrently
         tokio::task::spawn(async move {
-            // Create a new HandlerService here, not sure if this is ideal for each request yet
-            let hs = HandlerService { config: config_clone };
+            let hs = HandlerService { config: config_clone }; // Create a new HandlerService here
             if let Err(err) = http1::Builder::new()
                 .serve_connection(io, service_fn(|req| hs.call(req)))
                 .await
@@ -65,8 +67,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
 #[derive(Clone)]
 struct HandlerService {
-    // Use Arc to make proxyConfig shareable among tasks
-    config: Arc<ProxyConfig>,
+    config: Arc<ProxyConfig>,  // Use Arc to make proxyConfig shareable among tasks
 }
 
 impl HandlerService {
@@ -74,11 +75,59 @@ impl HandlerService {
         let uri = req.uri().path();
 
         if let Some(route) = self.config.routes.get(uri) {
-            // a simluation of a proxy request
-            let resp_body = format!("Routing to {}:{}", route.host, route.port);
-            Ok(Response::new(Full::new(Bytes::from(resp_body))))
-        } else {
-            Ok(Response::new(Full::new(Bytes::from("Route not found"))))
+            // Search for HeaderAdderConfig in the filters
+            let mut header_adder_config: Option<HeaderAdderConfig> = None;
+            for filter in &route.filters {
+                // this is an irrefutable if let for now, but it will be refutable in the future?
+                if let FilterConfig::HeaderAdder(config) = filter {
+                    header_adder_config = Some(config.clone());
+                    break;
+                }
+            }
+
+            // if a HeaderAdderConfig was found, use it, else proceed without it
+            if let Some(header_adder_config) = header_adder_config {
+                // Create a new HeaderAdderService with config
+                let header_adder = HeaderAdderService { config: header_adder_config.into() };
+
+                // a simulation of a proxy request
+                let resp_body = format!("Routing to {}:{}", route.host, route.port);
+                let response = Response::new(Full::new(Bytes::from(resp_body)));
+
+                // adds the custom header and returns, for now
+                return header_adder.add_header(response).await;
+            } else {
+                // HeaderAdderConfig not found, proceed without it
+                let resp_body = format!("Routing to {}:{}", route.host, route.port);
+                return Ok(Response::new(Full::new(Bytes::from(resp_body))));
+            }
         }
+
+        // if all else fails, return a 404
+        let returned_resp = Response::builder()
+            .status(404)
+            .body(Full::new(Bytes::from("404 Not Found")))
+            .unwrap();
+
+        Ok(returned_resp)
+
+    }
+}
+
+#[derive(Clone)]
+struct HeaderAdderService {
+    config: Arc<HeaderAdderConfig>,
+}
+
+impl HeaderAdderService {
+    async fn add_header(&self, mut response: Response<Full<Bytes>>) -> Result<Response<Full<Bytes>>, Infallible> {
+        let header_name = HeaderName::from_str(&self.config.header_key).unwrap();
+        let header_value = HeaderValue::from_str(&self.config.header_value).unwrap();
+        
+        response.headers_mut().insert(
+            header_name,
+            header_value,
+        );
+        Ok(response)
     }
 }
